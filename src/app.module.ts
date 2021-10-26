0ba790e1f248b86db679efa570a4baf944c18bd0
katlego-Mt <moimakatlego@outlook.com>
2021-10-26 14:42:20 +0200

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,10 @@ import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Onlinestore', { autoCreate: true }),
-    ProductModule],
-  controllers: [AppController, ProductController],
-  providers: [AppService, ProductService],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Onlinestore', { autoCreate: true }),ProductModule],
+   
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
 
