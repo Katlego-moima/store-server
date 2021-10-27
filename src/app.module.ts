34f88f05f9a1c7669c6affc58fdc3a3a84dac29c
katlego-Mt <moimakatlego@outlook.com>
2021-10-27 10:41:53 +0200

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductController } from './product/product.controller';
-import { ProductService } from './product/product.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Onlinestore', { autoCreate: true }),ProductModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Onlinestore', { autoCreate: true }),
+  ProductModule],
    
   controllers: [AppController],
   providers: [AppService],
