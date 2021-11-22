335105c64d29486744408dc119c0d4385c7a161c
Kat <moimakatlego2@gmail.com>
2021-11-22 23:28:57 +0200

    authentication added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductController } from './product/product.controller';
-import { ProductService } from './product/product.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Onlinestore', { autoCreate: true }),ProductModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Onlinestore', { autoCreate: true }),ProductModule, AuthModule],
    
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
 
 
 
